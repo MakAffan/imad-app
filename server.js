a289264ddea240a252e7e77a555c4098ff6da653
MakAffan <34371337+MakAffan@users.noreply.github.com>
2018-03-02 20:51:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/test-db',function(req,res){
 
 app.get('/article/:articleName',function(req,res){
     var articleName=req.params.articleName;
-    pool.query("SELECT * FROM article WHERE TITLE = " + req.params.articleName,function(err,result){
+    pool.query("SELECT * FROM article WHERE TITLE = $1",[req.params.articleName],function(err,result){
         if(err){
          res.status(500).send(err.toString);
       }else{
